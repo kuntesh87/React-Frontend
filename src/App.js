ee9f7ba0d8a93e1a75f688af714eb9912afe6586
Kuntesh Patel <khpatel@gmail.com>
2020-07-02 14:55:01 -0400

    init graphql

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const EXCHANGE_RATES = gql`
+  {
+  books{
+    name,
+    genre
+  }
 }
+`;
 
-export default App;
+export default function ExchangeRates() {
+  const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.books.map(({ name, genre }) => (
+    <div key={name}>
+      <p>
+        {name}: {genre}
+      </p>
+    </div>
+  ));
+}
